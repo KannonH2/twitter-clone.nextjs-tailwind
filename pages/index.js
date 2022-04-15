3c7fc2fb8240206a79bcccd1fbe3fabe98148c9d
KannonH2 <manaxtatu@gmail.com>
2022-04-15 02:15:14 -0300

    Comments Modal

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import Login from '../components/Login'
-import { getProviders, getSession, useSession } from "next-auth/react";
+import Modal from '../components/Modal'
+import { getProviders, getSession, useSession } from 'next-auth/react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 
-export default function Home ({trendingResults, followResults, providers}) {
-  const {data : session} = useSession();
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession()
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
 
-  if(!session) return <Login providers={providers} />
+  if (!session) return <Login providers={providers} />
 
   return (
     <div>
@@ -16,30 +20,25 @@ export default function Home ({trendingResults, followResults, providers}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
         <Sidebar />
-        <Feed/>
+        <Feed />
         {/* widgets */}
-
-        {/* modal */}
+        {isOpen && <Modal />}
       </main>
-
     </div>
   )
 }
 
-
-
-
-export async function getServerSideProps (context) {
-  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
     (res) => res.json()
-  );
-  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+  )
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
     (res) => res.json()
-  );
-  const providers = await getProviders();
-  const session = await getSession(context);
+  )
+  const providers = await getProviders()
+  const session = await getSession(context)
 
   return {
     props: {
@@ -48,6 +47,5 @@ export async function getServerSideProps (context) {
       providers,
       session,
     },
-  };
+  }
 }
-
